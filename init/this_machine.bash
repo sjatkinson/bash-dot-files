10b353a69f92bb7375902e9a240ef513477eeb9f
Atkinson <atkinson.steven@gmail.com>
2018-04-07 14:43:33 -0700

    fix syntax error in conditional

--- init/this_machine.bash
+++ init/this_machine.bash
@@ -1,7 +1,7 @@
 this_machine=`uname -n`
 this_machine_script=${init_dir}/machines/${this_machine%%.*}.bash
 echo $this_machine_script
-if [[ -e $this_machine_script ]] ; then
+if [ -e $this_machine_script ] ; then
     source $this_machine_script
 else
     echo !! No script for this computer: $this_machine_script !!
